a231c6461d8dad6e04dca738f3c72fbc0c1041f7
ChrolloRifat <officialmhrifat@outlook.com>
2025-02-18 00:37:56 +0600

    cleanup the declaration of the zen browser

--- modules/programs/browser/zen-browser/default.nix
+++ modules/programs/browser/zen-browser/default.nix
@@ -6,7 +6,7 @@
 { config, pkgs, inputs, zen-browser, ... }:
 {
       environment.systemPackages = with pkgs; [
-        inputs.zen-browser.packages."x86_64-linux".zen-browser
+        zen-browser
 
       ];
 }
