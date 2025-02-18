093e28a54e713ec4906553ae34426958286d5edd
ChrolloRifat <officialmhrifat@outlook.com>
2025-02-19 01:58:39 +0600

    Cleans up the fonts module

--- modules/programs/fonts/default.nix
+++ modules/programs/fonts/default.nix
@@ -2,9 +2,6 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-#let
-#  siyamrupali = pkgs.callPackage ./siyamrupali { inherit pkgs };
-#in
 { config, pkgs, ... }:
 
 {
