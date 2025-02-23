2a052a2715f2e29eb524fc32249c2c869a3b75d1
ChrolloRifat <officialmhrifat@outlook.com>
2025-02-23 17:47:06 +0600

    Edit some fonts

--- modules/programs/fonts/default.nix
+++ modules/programs/fonts/default.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, inputs, ndots, ... }:
 
 {
@@ -13,7 +9,7 @@
 
   # Running X11 applications or any other program uses Fontconfig.
   # A config file pointing to a set of default fonts
-  #fonts.fontconfig.enable = true;
+  fonts.fontconfig.enable = true;
 
   # Enables bitmap support
   fonts.fontconfig.useEmbeddedBitmaps = true;
@@ -21,7 +17,20 @@
   # Some "basic" fonts to be installed for reasonable Unicode coverage
   # Enable if you are unsure about what languages you might end up reading
   fonts.enableDefaultPackages = true;
-
+  fonts.fontconfig.defaultFonts = {
+      monospace = [
+        "DejaVu Sans Mono"
+        "IPAGothic"
+      ];
+      sansSerif = [
+        "DejaVu Sans"
+        "IPAPGothic"
+      ];
+      serif = [
+        "DejaVu Serif"
+        "IPAPMincho"
+      ];
+    };
 
   fonts.packages = with pkgs; [
 	hack-font
@@ -31,15 +40,29 @@
 	nerd-fonts.victor-mono
 	nerd-fonts.iosevka
 	nerd-fonts.iosevka-term
-	udev-gothic-nf			# Programming font with UD Gothic, JBrains and NF
-	ipaexfont 		    	# JP font with Mincho and Gothic fonts
-	noto-fonts-cjk-sans	  # Sans style between modern and traditional Chinese,Japanese,Korean fonts
-  noto-fonts-cjk-serif		# Serif style between modern and traditional Chinese,Japanese,Korean fonts
-  kanji-stroke-order-font		# Contains diagrams for 6500+ kanjis and more
-  #(callPackage ./siyamrupali/default.nix { inherit pkgs; })            # Siyam Rupali Bangla Font(have issues rn)
+	udev-gothic-nf	# Programming font with UD Gothic, JBrains and NF
+	ipaexfont  # JP font with Mincho and Gothic fonts
+	noto-fonts-cjk-sans	# Sans style between modern and traditional Chinese,Japanese,Korean fonts
+  noto-fonts-cjk-serif # Serif style between modern and traditional Chinese,Japanese,Korean fonts
+  kanji-stroke-order-font	 # Contains diagrams for 6500+ kanjis and more
+  material-design-icons # 7k+ material design icons from the community
+  font-awesome_5
+  migmix  # High Quality JP font based on M+ and IPA fonts
+  hachimarupop  # Cute Japanese Font
+  #(callPackage ./siyamrupali/default.nix { inherit pkgs; })  # Siyam Rupali Bangla Font(have issues rn)
   #siyamrupali
   inputs.ndots.packages."x86_64-linux".road-rage
 
   ];
+
+  # Ensure the Fontconfig cache is updated after the fonts are installed
+  systemd.services.update-font-cache = {
+    description = "Update Fontconfig cache";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.fontconfig}/bin/fc-cache -f -v";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 
 }
