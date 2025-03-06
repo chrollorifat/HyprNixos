0771312d8195780a946a88ab89e30f43712761b4
ChrolloRifat <officialmhrifat@outlook.com>
2025-03-06 14:40:36 +0600

    Disable some fcitx addons

--- modules/programs/misc/fcitx/default.nix
+++ modules/programs/misc/fcitx/default.nix
@@ -8,10 +8,10 @@
       fcitx5-skk
       fcitx5-lua
       fcitx5-gtk
-      fcitx5-tokyonight
+      # fcitx5-tokyonight
       catppuccin-fcitx5
-      fcitx5-mellow-themes
-      kdePackages.fcitx5-qt
+      # fcitx5-mellow-themes
+      # kdePackages.fcitx5-qt
       kdePackages.fcitx5-configtool
       fcitx5-m17n 
     ];
