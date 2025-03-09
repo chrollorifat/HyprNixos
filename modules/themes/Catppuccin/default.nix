8e8d115529a5413029e2121ced71a50649c3d51c
ChrolloRifat <officialmhrifat@outlook.com>
2025-03-10 05:14:51 +0600

    Cleanup the code in catppuccin theme module

--- modules/themes/Catppuccin/default.nix
+++ modules/themes/Catppuccin/default.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   pkgs,
   wallpaper,
   ...
@@ -8,43 +9,10 @@
   catppuccin-kvantum-pkg = pkgs.catppuccin-kvantum.override {inherit variant accent;};
   catppuccin = "catppuccin-${variant}-${accent}";
 
-  catppuccin-gtk = pkgs.catppuccin-gtk.overrideAttrs {
-    src = pkgs.fetchFromGitHub {
-      owner = "catppuccin";
-      repo = "gtk";
-      rev = "v1.0.3";
-      fetchSubmodules = true;
-      hash = "sha256-q5/VcFsm3vNEw55zq/vcM11eo456SYE5TQA3g2VQjGc=";
-    };
-    postUnpack = "";
-  };
 in {
   home-manager.sharedModules = [
     ({config, ...}: {
       home.packages = [catppuccin-kvantum-pkg];
-
-      # Set wallpaper
-      services.hyprpaper = {
-        enable = true;
-        settings = {
-          preload = ["${../wallpapers/${wallpaper}}"];
-          wallpaper = [",${../wallpapers/${wallpaper}}"];
-        };
-      };
-
-      dconf.settings = {
-        "org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
-        };
-      };
-
-      home.pointerCursor = {
-        gtk.enable = true;
-        x11.enable = true;
-        package = pkgs.bibata-cursors;
-        name = "Bibata-Modern-Classic";
-        size = 24;
-      };
 
       qt = {
         enable = true;
@@ -55,10 +23,10 @@ in {
       gtk = {
         enable = true;
         theme = {
-          name = "${catppuccin}-compact";      
-          package = catppuccin-gtk.override {
-            accents = [accent];
+          name = "${catppuccin}-compact";
+          package = pkgs.catppuccin-gtk.override {
             variant = variant;
+            accents = [accent];
             size = "compact";
           };
         };
@@ -80,6 +48,30 @@ in {
           '';
         };
       };
+
+      # Set wallpaper
+      services.hyprpaper = {
+          enable = true;
+          settings = {
+           preload = ["${../wallpapers/${wallpaper}}"];
+           wallpaper = [",${../wallpapers/${wallpaper}}"];
+          };
+      };
+
+      dconf.settings = {
+          "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+           };
+      };
+
+      home.pointerCursor = {
+          gtk.enable = true;
+          x11.enable = true;
+          package = pkgs.bibata-cursors;
+          name = "Bibata-Modern-Classic";
+          size = 24;
+      };
+
       xdg.configFile = {
         "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
         "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
