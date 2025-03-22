9f1308f2947042ca898c38f6eca9926da539b1c3
ChrolloRifat <officialmhrifat@outlook.com>
2025-03-22 06:24:21 +0600

    Add mimeApps for mpv & resume support

--- modules/programs/media/mpv/default.nix
+++ modules/programs/media/mpv/default.nix
@@ -111,6 +111,7 @@
           # T = "script-binding generate-thumbnails";
         };
         config = {
+          save-position-on-quit = "yes";
           osc = "no";
           watch-later-directory = "${config.xdg.stateHome}/mpv/watch_later";
           resume-playback-check-mtime = true;
@@ -129,6 +130,26 @@
           screenshot-sw = true;
           cache-dir = "${config.xdg.cacheHome}/mpv";
           input-default-bindings = false;
+          ytdl-format = "bestvideo+bestaudio";
+        };
+        xdg.mimeApps.defaultApplications = {
+          "video/mp4" = "mpv.desktop";
+          "video/x-matroska" = "mpv.desktop";
+          "video/webm" = "mpv.desktop";
+          "video/quicktime" = "mpv.desktop";
+          "video/x-msvideo" = "mpv.desktop";
+          "video/x-ms-wmv" = "mpv.desktop";
+          "video/x-flv" = "mpv.desktop";
+          "video/x-m4v" = "mpv.desktop";
+          "video/3gpp" = "mpv.desktop";
+          "video/3gpp2" = "mpv.desktop";
+          "video/x-matroska-3d" = "mpv.desktop";
+          "video/x-ms-asf" = "mpv.desktop";
+          "video/x-ms-wvx" = "mpv.desktop";
+          "video/x-ms-wmx" = "mpv.desktop";
+          "video/x-ms-wm" = "mpv.desktop";
+          "video/x-ms-wmp" = "mpv.desktop";
+          "video/x-ms-wmz" = "mpv.desktop";
         };
       };
     })
